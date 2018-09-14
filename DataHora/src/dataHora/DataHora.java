93ffb1a7db5482e3461f6c8ebf4130a04999903c
NatanGds <pcnatan@live.com>
2018-09-13 22:25:25 -0300

    Parte inicial do sistema Data\Hora

---
+++ DataHora/src/dataHora/DataHora.java
@@ -0,0 +1,16 @@
+package dataHora;
+
+public class DataHora {
+	
+	private byte dia;
+	private byte mes;
+	private byte ano;
+	
+	public data(byte dia, byte mes, byte ano){
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+	}
+	
+
+}
